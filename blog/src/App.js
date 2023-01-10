1466d158d256422e64e5eeb3743cca5cfc28ae1c
leeshahub <leesha1223@naver.com>
2023-01-10 22:23:49 +0900

    lesson 3 state

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목,b] = useState(['강남 우동 추천','신사 우동 추천','논현 우동 추천']);
+  let [logo, setLogo] = useState('ReactBlog');
+  
+  // let num = [1,2];
+  // let [a,c] = [1,2];
+  // let a = num[0];
+  // let c = num[1];
 
   return (
     <div className="App">
      <div className="black-nav">
-      <h4 style={ {color:'red', fontSize : '16px'} }id={post}>블로그임</h4>
+      <h4 id={post}>ReactBlog</h4>
      </div>
+      <div className="list">
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
      <h4>{post}</h4>
     </div>
   );
